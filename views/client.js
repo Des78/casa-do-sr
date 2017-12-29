093639a9cfc462dfc62d2fa84bb400c63763ea5b
Glitch (casa-do-sr) <none>
2017-12-29 18:44:08 +0000

    Initial import

---
+++ views/client.js
@@ -0,0 +1,23 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+// by default, you've got jQuery,
+// add other scripts at the bottom of index.html
+
+function turnLightsOn()
+{
+  $.post('/LightsOn');
+  alert("ON");
+}
+
+function turnLightsOff()
+{
+  //alert("SwitchingOff");
+  $.post('/LightsOut');
+  alert("OFF");
+}
+
+function triggerTest()
+{
+  $.post('/test');
+}
