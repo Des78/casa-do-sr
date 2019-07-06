f6c82fdc8a930410e758f07d03ff11ba253df513
Des78 <joas@fe.up.pt>
2019-07-06 10:38:40 +0100

    workaround for servers with no DST

--- services/utilities.js
+++ services/utilities.js
@@ -33,7 +33,13 @@ exports.isDst = () => {
     let now = new Date();
     let jan = new Date(now.getFullYear(), 0, 1);
     let jul = new Date(now.getFullYear(), 6, 1);
+    let isDs = now.getTimezoneOffset() < Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset())
+
+    if (jan.getTimezoneOffset() == jul.getTimezoneOffset()) {
+        // server doesn't have DST, make a guess
+        isDs = (now.getMonth() > 3 && now.getMonth() < 11);
+    }
     
-    return now.getTimezoneOffset() < Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+    return isDs;
 }
 
